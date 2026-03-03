d164f599eb885a04fb23381b001df7d276f55b3a
Akhilkumar0808 <mattupalliakhil0815@gmail.com>
2026-03-03 12:20:38 +0530

    Changes

--- src/auth/ProtectedRoute.jsx
+++ src/auth/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children }) {
   const isAuth = localStorage.getItem("isAuth");
-  return isAuth ? children : <Navigate to="/login" />;
+  return isAuth ? children : <Navigate to="/user-login" />;
 }
 
 export default ProtectedRoute;
