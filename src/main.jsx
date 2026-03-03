90b209e14565192f8f3bbb2f76e556c5352d7e46
Akhilkumar0808 <mattupalliakhil0815@gmail.com>
2026-03-03 11:58:50 +0530

    Third changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
-import React,{ StrictMode } from 'react'
+// import React,{ StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import { BrowserRouter,HashRouter } from 'react-router-dom'
+
+// createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter,HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>,
 )
